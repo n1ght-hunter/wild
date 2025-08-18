8f481305ee8e2d8224d2cdf883425302918a4764
Night_Hunter <samuelhuntnz@gmail.com>
2025-08-19 03:30:09 +1200

    add tracing-subscriber for enhanced logging and include new dependencies

--- windows/wlibwild/src/bin/main.rs
+++ windows/wlibwild/src/bin/main.rs
@@ -1,7 +1,20 @@
+use tracing_subscriber::{
+    Layer as _, fmt::format::FmtSpan, layer::SubscriberExt as _, util::SubscriberInitExt as _,
+};
 use wlibwild::run;
 
-
-
 fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::fmt::layer()
+                .with_line_number(true)
+                .with_file(true)
+                .with_span_events(FmtSpan::CLOSE)
+                .with_filter(
+                    tracing_subscriber::EnvFilter::try_from_default_env()
+                        .unwrap_or("wlibwild=info,warn".into()),
+                ),
+        )
+        .init();
     run();
 }
--- windows/wlibwild/src/lib.rs
+++ windows/wlibwild/src/lib.rs
@@ -1,7 +1,17 @@
+use std::path::{Path, PathBuf};
+
+use tracing::{info, warn};
+
 
 
 pub fn run() {
-    
+    let files = vec![
+        PathBuf::from("basic_objs/function.o"),
+        PathBuf::from("basic_objs/main.o"),
+    ];
+    link_files(files);
 }
 
-
+fn link_files(files: Vec<PathBuf>) {
+    info!("Starting to link files: {:?}", files);
+}
